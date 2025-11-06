6c27b09c4b7d080d8d74ef2be38eef4d70ecd266
Brian Mitchell <dr.brian.mitchell@gmail.com>
2025-11-06 17:02:20 -0500

    moved to drexel cci

--- hw3-http/http.h
+++ hw3-http/http.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 #define     DEFAULT_HOST    "httpbin.org"
-#define     DEFAULT_HOST_2  "ec2-98-94-0-172.compute-1.amazonaws.com"
+#define     DEFAULT_HOST_2  "cci-p141.cci.drexel.edu"
 #define     DEFAULT_PORT    80
 #define     DEFAULT_PATH    "/"
 
